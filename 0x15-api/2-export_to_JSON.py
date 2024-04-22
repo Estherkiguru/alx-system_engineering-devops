22491e9f00409682c32f3dcc108df8ddab6fc987
EstherKiguru <esipraisewambui@gmail.com>
2024-04-22 11:49:29 -0700

    API

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,9 +2,9 @@
 """
 returns information about employees TODO list progress
 """
+import json
 import requests
 import sys
-import json
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
