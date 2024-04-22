2ed612f0272ce854b136700e8f8b20a87210f793
EstherKiguru <esipraisewambui@gmail.com>
2024-04-22 11:09:20 -0700

    API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
 Exports to-do list information of all employees to JSON format.
-This script fetches the user information and to-do lists for all employees
-from the JSONPlaceholder API and exports the data to a JSON file
 """
 
 import json
@@ -11,13 +9,10 @@ import requests
 
 def fetch_user_data():
     """Fetch user information and to-do lists for all employees."""
-    # Base URL for the JSONPlaceholder API
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch the list of all users (employees)
     users = requests.get(url + "users").json()
 
-    # Create a dictionary containing to-do list information of all employees
     data_to_export = {}
     for user in users:
         user_id = user["id"]
@@ -39,6 +34,5 @@ def fetch_user_data():
 if __name__ == "__main__":
     data_to_export = fetch_user_data()
 
-    # Write the data to a JSON file
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump(data_to_export, jsonfile, indent=4)
