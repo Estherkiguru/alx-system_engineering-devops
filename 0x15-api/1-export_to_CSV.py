fcaf8a6478bab03a087279761d857486c204027d
EstherKiguru <esipraisewambui@gmail.com>
2024-04-22 19:17:58 -0700

    API corrections task1 again

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,41 +2,27 @@
 """
 returns information about employees TODO list progress
 """
+import csv
 import requests
 import sys
-import csv
 
-
-import requests
-import sys
-import csv
 
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
 
     user_id = sys.argv[1]
 
-    # Fetch user data
-    user_response = requests.get(url + "users/{}".format(user_id))
-    if user_response.status_code != 200:
-        print("Failed to fetch user data")
-        sys.exit(1)
-    user_data = user_response.json()
-    username = user_data.get("username")
+    user = requests.get(url + "users/{}".format(user_id)).json()
 
-    # Fetch todos for the user
-    todos_response = requests.get(url + "todos", params={"userId": user_id})
-    if todos_response.status_code != 200:
-        print("Failed to fetch TODO list")
-        sys.exit(1)
-    todos = todos_response.json()
+    username = user.get("username")
 
-    # Write data to CSV
+    params = {"userId": user_id}
+
+    todos = requests.get(url + "todos", params).json()
+
     with open("{}.csv".format(user_id), "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
 
         for t in todos:
-            writer.writerow([user_id, username, t.get("completed"), t.get("title")])
-
-    print("Tasks exported to {}.csv".format(user_id))
-
+            writer.writerow([user_id, username, t.get("completed"),
+                            t.get("title")])
